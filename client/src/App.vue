54f5596b46975b3dce95e8c0e3a5cd00e4791b63
yejinzhan <jinzhan.ye@foxmail.com>
2023-03-03 22:48:42 +0800

    lesson-11

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,73 @@
 <template>
   <div>
+    <el-button @click="showAddAuthorDialog">添加作者</el-button>
+  </div>
+  <div>
     <el-table :data="booksResult?.books">
       <el-table-column prop="name" label="书名"/>
       <el-table-column prop="type" label="类型"/>
       <el-table-column prop="author.name" label="作者"/>
     </el-table>
   </div>
+
+  <el-dialog v-model="authorDialogVisible">
+    <el-form :model="authorFormData">
+      <el-form-item label="姓名">
+        <el-input v-model="authorFormData.name"/>
+      </el-form-item>
+
+      <el-form-item label="性别">
+        <el-radio-group v-model="authorFormData.gender">
+          <el-radio :label="1">男</el-radio>
+          <el-radio :label="2">女</el-radio>
+        </el-radio-group>
+      </el-form-item>
+
+      <el-form-item label="邮箱">
+        <el-input v-model="authorFormData.email"/>
+      </el-form-item>
+    </el-form>
+
+    <template #footer>
+      <el-button type="primary"
+                 @click="onSubmitAuthor()">
+        提交
+      </el-button>
+    </template>
+  </el-dialog>
 </template>
 
 <script setup lang="ts">
-import { useBooks } from './service/index'
+import { useBooks, useAddAuthor, useAuthors } from './service/index'
+import { ref } from 'vue'
+import { ElMessage } from 'element-plus'
 
 const { result: booksResult } = useBooks()
+const { result: authorsResult, refetch: refetchAuthors } = useAuthors()
+
+
+const authorDialogVisible = ref(false)
+const authorFormData = ref({})
+
+const showAddAuthorDialog = () => {
+  authorFormData.value = {}
+  authorDialogVisible.value = true
+}
+
+const onSubmitAuthor = () => {
+  const { mutate, onDone }  = useAddAuthor()
+  mutate({
+    createAuthorInput: {
+      name: authorFormData.value.name,
+      gender: authorFormData.value.gender,
+      email: authorFormData.value.email
+    }
+  })
+
+  onDone(() => {
+    refetchAuthors()
+    ElMessage.success('操作成功')
+    authorDialogVisible.value = false
+  })
+}
 </script>
--- client/src/service/index.ts
+++ client/src/service/index.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@vue/apollo-composable'
+import { useQuery, useMutation } from '@vue/apollo-composable'
 import gql from 'graphql-tag'
 
 export const useBooks = () => {
@@ -18,4 +18,32 @@ export const useBooks = () => {
   return {
     result
   }
+}
+
+export const useAuthors = () => {
+  const { result, refetch } = useQuery(gql`
+    query getAuthors {
+      authors {
+        id
+        name
+      }
+    }
+  `)
+
+  return {
+    result,
+    refetch
+  }
+}
+
+export const useAddAuthor = () => {
+  const { mutate, onDone } = useMutation(gql`
+    mutation createAuthor($createAuthorInput: CreateAuthorInput!) {
+      createAuthor(createAuthorInput: $createAuthorInput) {
+        id
+      }
+    }
+  `)
+
+  return { mutate, onDone }
 }
